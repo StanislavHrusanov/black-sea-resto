fc55d35bb35a9f12d54eb6931b25489c4adbc4be
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-09 18:35:38 +0200

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+    return (
+        <header>
+            <h1><Link className="home" to="/">Black See Resto</Link></h1>
+            <nav>
+                <Link to="/restaurants">Restaurants</Link>
+
+                <div id="user">
+                    <Link to="/addRestaurant">Add restaurant</Link>
+                    <Link to="/logout">Logout</Link>
+                </div>
+
+                <div id="guest">
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>
+            </nav>
+        </header>
+    );
+}
