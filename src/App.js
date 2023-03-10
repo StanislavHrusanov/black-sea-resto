0ca6cafc6a9bbcc5d837c10187007d90b3b23afa
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-10 15:27:46 +0200

    Update App.css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
 
 import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
 
 function App() {
   return (
     <div >
 
       <Header />
+
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+      </Routes>
 
 
     </div>
