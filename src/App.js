21f6be8801ca965953f2aa0699eca3cb801dd8b4
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-10 21:01:53 +0200

    Add Auth css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
+import { Register } from './components/Auth/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
 
         <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
 
 
