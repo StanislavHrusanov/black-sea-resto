3d85a9b9ab167d7100b798e9d6c64c22cf0ece44
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-14 10:34:16 +0200

    Show correct Navigation according to user's value

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,29 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css"
 
+import { AuthContext } from "../../contexts/AuthContext";
+
 export const Header = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <header>
             <h1><Link className={styles["home"]} to="/">Black See Resto</Link></h1>
             <nav>
                 <Link to="/restaurants">Restaurants</Link>
+                {user
+                    ? <div id="user">
+                        <Link to="/addRestaurant">Add restaurant</Link>
+                        <Link to="/logout">Logout</Link>
+                        <span>{`Hello, ${user.username}`}</span>
+                    </div>
 
-                <div id="user">
-                    <Link to="/addRestaurant">Add restaurant</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                    : <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>
+                }
             </nav>
         </header>
     );
