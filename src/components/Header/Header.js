8d49776d153f3d797a6127ec5a85719b8d2ddd1c
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-14 14:25:45 +0200

    Add logout functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,38 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Header.module.css"
 
 import { AuthContext } from "../../contexts/AuthContext";
+import * as authService from "../../services/authService";
 
 export const Header = () => {
-    const { user } = useContext(AuthContext);
+    const { user, userLogout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onLogout = async (e) => {
+        e.preventDefault();
+        try {
+            await authService.logout();
+            userLogout();
+            navigate('/');
+
+        } catch (error) {
+            return window.alert(error);
+        }
+    }
 
     return (
         <header>
             <h1><Link className={styles["home"]} to="/">Black See Resto</Link></h1>
             <nav>
+                {user &&
+                    <span className={styles["hello"]}>{`Hello, ${user.username}!`}</span>
+                }
                 <Link to="/restaurants">Restaurants</Link>
                 {user
                     ? <div id="user">
                         <Link to="/addRestaurant">Add restaurant</Link>
-                        <Link to="/logout">Logout</Link>
-                        <span>{`Hello, ${user.username}`}</span>
+                        <Link to="/logout" onClick={(e) => onLogout(e)}>Logout</Link>
                     </div>
 
                     : <div id="guest">
