a404cd329916c547b41c7b3986d6e7cd517dc668
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-16 15:31:19 +0200

    Add Review component and css

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,9 +3,11 @@ import { useState } from "react";
 import styles from "./Details.module.css";
 
 import { AddReview } from "./AddReview/AddReview";
+import { Review } from "./Review/Review";
+
 export const Details = () => {
     const [showModal, setShowModal] = useState(false);
-    const rating = 2;
+    const rating = 3;
 
     const openModal = () => {
         setShowModal(true)
@@ -14,19 +16,17 @@ export const Details = () => {
     const closeModal = () => {
         setShowModal(false);
     }
+
     return (
         <section className={styles["details-page"]}>
             {showModal &&
                 <AddReview onCloseModal={closeModal} />
             }
-            {/* <div className={styles["details-message"]}>
-                <h2>We will help you find your perfect restaurant!</h2>
-            </div> */}
 
             <div className={styles["item-details"]}>
 
                 <div className={styles["item-description"]}>
-                    <h2> A Game Of Thrones</h2>
+                    <h2> Azahar</h2>
                     <h3><div className={styles["rating"]}>
                         {[...Array(5)].map((star, index) => {
                             index += 1;
@@ -43,36 +43,23 @@ export const Details = () => {
                         <span>{`${rating.toFixed(1)} (2) rewiews`}</span>
 
                     </div></h3>
-                    <h3>Address: George R.R Martin</h3>
-                    <h3>Phone: 5</h3>
-                    <h3>Capacity: Fiction</h3>
-                    <h3>Summary: alabalaczcz dzzdzc czczczczxczxc czczczczx czczcsfcsdczxc  czczczczc dzczcz adczccscssssssssssss dsaaaaaa daaaaaaaaaaaa alabalaczcz dzzdzc czczczczxczxc czczczczx czczcsfcsdczxc  czczczczc dzczcz adczccscssssssssssss dsaaaaaa daaaaaaaaaaaaalabalaczcz dzzdzc czczczczxczxc czczczczx czczcsfcsdczxc  czczczczc dzczcz adczccscssssssssssss dsaaaaaa daaaaaaaaaaaaalabalaczcz dzzdzc czczczczxczxc czczczczx czczcsfcsdczxc  czczczczc dzczcz adczccscssssssssssss dsaaaaaa daaaaaaaaaaaaalabalaczcz dzzdzc czczczczxczxc czczczczx czczcsfcsdczxc  czczczczc dzczcz adczccscssssssssssss dsaaaaaa daaaaaaaaaaaa</h3>
+                    <h3>Address: dadada</h3>
+                    <h3>Phone: 321221313</h3>
+                    <h3>Capacity: 212</h3>
+                    <h3>Summary: alabalaczcz dzzdzc czczczczxczxc czczczczx czczcsfcsdczxc  c</h3>
                     <div className={styles["buttons"]}>
-                        <a href="#" className={styles["edit-button"]}>Edit</a>
-                        <a href="#" className={styles["delete-button"]}>Delete</a>
-                        <a className={styles["favourite-button"]} onClick={() => openModal()}>Favourite</a>
-                        {/* <p className="wish-pub">You already added the book to your wish list</p> */}
+                        <Link to="#" className={styles["edit-button"]}>Edit</Link>
+                        <Link to="#" className={styles["delete-button"]}>Delete</Link>
+                        <Link className={styles["favourite-button"]} >Favourite</Link>
+                        {/* <p className="wish-pub"></p> */}
                     </div>
 
-                    <div className={styles["rating-container"]}>
+                    <div className={styles["reviews-container"]}>
 
-                        <div className={styles["rating"]}>
-                            {[...Array(5)].map((star, index) => {
-                                index += 1;
-                                const rating = 2;
+                        <button onClick={() => openModal()} className={styles["add-review-btn"]}>Add review</button>
 
-                                return (
-                                    <span
-                                        key={index}
-                                        className={index <= Math.round(rating) ? styles["full"] : styles["empty"]}
-                                    >
-                                        ☆
-                                    </span>
-                                )
-                            })}
-                            {/* <p>{`(${restaurant.reviews.length}) rewiews`}</p> */}
+                        {<Review rating={rating} />}
 
-                        </div>
                     </div>
 
                 </div>
@@ -80,8 +67,6 @@ export const Details = () => {
                 <div className={styles["item-details-image"]}>
                     <img src="https://static.pochivka.bg/bgstay.com/images/photos/39/39827/orig_39827_1.jpg" alt="resto" />
                 </div>
-
-
             </div>
         </section>
     );
