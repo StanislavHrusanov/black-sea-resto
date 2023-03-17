1a8b994ec4b7556da028672e993eebb875d48367
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-17 22:08:53 +0200

    Add function to edit restaurant

--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -3,7 +3,8 @@ import * as api from "./api";
 const endpoints = {
     allRestaurants: '/data/restaurants?sortBy=_createdOn%20desc',
     addRestaurant: '/data/restaurants',
-    getRestaurant: (restaurantId) => `/data/restaurants/${restaurantId}`
+    getRestaurant: (restaurantId) => `/data/restaurants/${restaurantId}`,
+    editRestaurant: (restaurantId) => `/data/restaurants/${restaurantId}`
 }
 
 export const getAllRestaurants = () => api.get(endpoints.allRestaurants);
@@ -11,3 +12,5 @@ export const getAllRestaurants = () => api.get(endpoints.allRestaurants);
 export const addRestaurant = (data) => api.post(endpoints.addRestaurant, data);
 
 export const getOne = (restaurantId) => api.get(endpoints.getRestaurant(restaurantId));
+
+export const edit = (restaurantId, data) => api.put(endpoints.editRestaurant(restaurantId), data);
