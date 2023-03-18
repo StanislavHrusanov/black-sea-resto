3f49967af482b409676c9fad3b04e941fe706cd7
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-18 15:59:07 +0200

    Add loadingSpinner to Home page and Restaurants page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,42 +1,50 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import styles from "./Home.module.css";
 
 import { Restaurant } from "./Restaurant/Restaurant";
 import { getAvgRating } from "../../utils";
 import { getAllRestaurants } from "../../services/restaurantService";
 
+import { LoadingContext } from "../../contexts/LoadingContext";
+import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
+
 export const Home = () => {
     const [restaurants, setRestaurants] = useState([]);
+    const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
 
     useEffect(() => {
+        showLoading();
         getAllRestaurants()
             .then(result => setRestaurants(result))
+            .then(() => hideLoading())
             .catch(err => window.alert(err.message));
-    }, []);
+    }, [showLoading, hideLoading]);
 
     const mostPopular = restaurants
         .slice()
         .sort((a, b) => b.reviews.length - a.reviews.length || getAvgRating(b.reviews) - getAvgRating(a.reviews))
         .slice(0, 3);
 
-    return (
-        <section id="home-section">
-            <div className={styles["home-message"]}>
-                <h2>We will help you find your perfect restaurant!</h2>
-            </div>
-            <div className={styles["home-container"]}>
-
-                <div className={styles["top-three"]}>
-                    <h1>Most Popular</h1>
-
-                    {
-                        mostPopular.length > 0
-                            ? mostPopular.map(x => <Restaurant key={x._id} restaurant={x} />)
-                            : <p className={styles["no-restaurants"]}>There is no restaurants yet!</p>
-                    }
-
+    return isLoading
+        ? (<LoadingSpinner />)
+        : (
+            <section id="home-section">
+                <div className={styles["home-message"]}>
+                    <h2>We will help you find your perfect restaurant!</h2>
                 </div>
-            </div>
-        </section>
-    );
+                <div className={styles["home-container"]}>
+
+                    <div className={styles["top-three"]}>
+                        <h1>Most Popular</h1>
+
+                        {
+                            mostPopular.length > 0
+                                ? mostPopular.map(x => <Restaurant key={x._id} restaurant={x} />)
+                                : <p className={styles["no-restaurants"]}>There is no restaurants yet!</p>
+                        }
+
+                    </div>
+                </div>
+            </section>
+        );
 }
--- src/components/Restaurants/Restaurants.js
+++ src/components/Restaurants/Restaurants.js
@@ -1,35 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import styles from "./Restaurants.module.css";
 
 import { RestaurantItem } from "./RestaurantItem/RestaurantItem";
 import { getAllRestaurants } from "../../services/restaurantService";
 
+import { LoadingContext } from "../../contexts/LoadingContext";
+import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
+
 export const Restaurants = () => {
     const [restaurants, setRestaurants] = useState([]);
+    const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
+
 
     useEffect(() => {
+        showLoading();
         getAllRestaurants()
             .then(result => setRestaurants(result))
+            .then(() => hideLoading())
             .catch(err => window.alert(err.message));
-    }, [])
+    }, [showLoading, hideLoading]);
 
-    return (
-        <section id="restaurants-section">
-            <div className={styles["restaurants-message"]}>
-                <h2>We will help you find your perfect restaurant!</h2>
-            </div>
-            <div className={styles["restaurants-container"]}>
-
-                <div className={styles["restaurants"]}>
-                    <h1>Restaurants</h1>
-
-                    {restaurants.length > 0
-                        ? restaurants.map(x => <RestaurantItem key={x._id} restaurant={x} />)
-                        : <p className={styles["no-restaurants"]}>There is no restaurants yet!</p>
-                    }
-
+    return isLoading
+        ? (<LoadingSpinner />)
+        : (
+            <section id="restaurants-section">
+                <div className={styles["restaurants-message"]}>
+                    <h2>We will help you find your perfect restaurant!</h2>
                 </div>
-            </div>
-        </section>
-    );
+                <div className={styles["restaurants-container"]}>
+
+                    <div className={styles["restaurants"]}>
+                        <h1>Restaurants</h1>
+
+                        {restaurants.length > 0
+                            ? restaurants.map(x => <RestaurantItem key={x._id} restaurant={x} />)
+                            : <p className={styles["no-restaurants"]}>There is no restaurants yet!</p>
+                        }
+
+                    </div>
+                </div>
+            </section>
+        );
 }
