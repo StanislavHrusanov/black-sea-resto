a6bb18c0b3fb4ba69470f9e4e554b3aebbba06ea
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-18 15:44:26 +0200

    Add LoadingSpinner component and css

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,18 @@
+import "./LoadingSpinner.css";
+
+export const LoadingSpinner = () => {
+    return (
+        <div className="center">
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+        </div>
+    );
+}
