c9e713ec272e2463199c906b0d01e4ffe8a2695d
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-19 17:55:27 +0200

    Add function to add reviews

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -1,7 +1,10 @@
 import * as api from "./api";
 
 const endpoints = {
-    getReviewsById: (restaurantId) => `/data/reviews?where=restaurantId%3D%22${restaurantId}%22`
+    getReviewsById: (restaurantId) => `/data/reviews?where=restaurantId%3D%22${restaurantId}%22`,
+    addReview: '/data/reviews'
 }
 
 export const getById = (restaurantId) => api.get(endpoints.getReviewsById(restaurantId));
+
+export const addReview = (review) => api.post(endpoints.addReview, review);
