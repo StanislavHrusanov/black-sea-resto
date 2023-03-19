49d2314d19223e61b8d82320b1105c419bbca358
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-19 11:36:50 +0200

    Add loading spinner to Edit page

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import styles from "./Edit.module.css";
 
 import * as restaurantService from "../../services/restaurantService";
+import { LoadingContext } from "../../contexts/LoadingContext";
+import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
 
 export const Edit = () => {
     const [restaurant, setRestaurant] = useState({
@@ -13,14 +15,20 @@ export const Edit = () => {
         imageUrl: '',
         summary: ''
     });
+    const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
     const { restaurantId } = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
+        showLoading();
         restaurantService.getOne(restaurantId)
             .then(result => setRestaurant(result))
-            .catch(err => window.alert(err.message));
-    }, [restaurantId]);
+            .then(() => hideLoading())
+            .catch(err => {
+                window.alert(err.message)
+                return navigate(`/restaurants/${restaurantId}`);
+            });
+    }, [restaurantId, showLoading, hideLoading, navigate]);
 
     const onChangeHandler = (e) => {
         setRestaurant(state => ({
@@ -41,94 +49,98 @@ export const Edit = () => {
         }
     }
 
-    return (
-        <section id="editRestaurant-page">
-            <div className={styles["container"]}>
-                <div>
-                    <h2 className={styles["heading"]}>
-                        Edit your restaurant
-                    </h2>
+    return isLoading
+        ? (
+            <LoadingSpinner />
+        )
+        : (
+            <section id="editRestaurant-page">
+                <div className={styles["container"]}>
+                    <div>
+                        <h2 className={styles["heading"]}>
+                            Edit your restaurant
+                        </h2>
+                    </div>
+
+                    <form onSubmit={onSubmit} className={styles["edit-form"]} >
+                        <div className={styles["input"]}>
+                            <label htmlFor="restaurant-name" className={styles["name"]}>Restaurant name</label>
+                            <input
+                                type="text"
+                                className={styles["input-field"]}
+                                name="name"
+                                id="restaurant-name"
+                                placeholder="Happy Bar & Grill"
+                                value={restaurant.name}
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <div className={styles["input"]}>
+                            <label htmlFor="restaurant-address" className={styles["address"]}>Address</label>
+                            <input
+                                type="text"
+                                className={styles["input-field"]}
+                                name="address"
+                                id="restaurant-address"
+                                placeholder="10, Vitosha, Sofia 1000"
+                                value={restaurant.address}
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <div className={styles["input"]}>
+                            <label htmlFor="phone-number" className={styles["phone"]}>Phone</label>
+                            <input
+                                type="text"
+                                className={styles["input-field"]}
+                                name="phone"
+                                id="phone-number"
+                                placeholder="+359888XXXXXX"
+                                value={restaurant.phone}
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <div className={styles["input"]}>
+                            <label htmlFor="restaurant-capacity" className={styles["capacity"]}>Capacity</label>
+                            <input
+                                type="text"
+                                className={styles["input-field"]}
+                                name="capacity"
+                                id="restaurant-capacity"
+                                placeholder="100 persons"
+                                value={restaurant.capacity}
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <div className={styles["input"]}>
+                            <label htmlFor="restaurant-image" className={styles["imageUrl"]}>Image</label>
+                            <input
+                                type="text"
+                                className={styles["input-field"]}
+                                name="imageUrl"
+                                id="restaurant-image"
+                                placeholder="http://image.jpeg"
+                                value={restaurant.imageUrl}
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <div className={styles["input"]}>
+                            <label htmlFor="restaurant-summary" className={styles["summary"]}>Summary</label>
+                            <input
+                                type="text"
+                                className={styles["input-field"]}
+                                name="summary"
+                                id="restaurant-summary"
+                                placeholder="Some text..."
+                                value={restaurant.summary}
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <div className={styles["action"]}>
+                            <button className={styles["action-button"]}>Edit</button>
+                        </div>
+                    </form>
+
                 </div>
-
-                <form onSubmit={onSubmit} className={styles["edit-form"]} >
-                    <div className={styles["input"]}>
-                        <label htmlFor="restaurant-name" className={styles["name"]}>Restaurant name</label>
-                        <input
-                            type="text"
-                            className={styles["input-field"]}
-                            name="name"
-                            id="restaurant-name"
-                            placeholder="Happy Bar & Grill"
-                            value={restaurant.name}
-                            onChange={onChangeHandler}
-                        />
-                    </div>
-                    <div className={styles["input"]}>
-                        <label htmlFor="restaurant-address" className={styles["address"]}>Address</label>
-                        <input
-                            type="text"
-                            className={styles["input-field"]}
-                            name="address"
-                            id="restaurant-address"
-                            placeholder="10, Vitosha, Sofia 1000"
-                            value={restaurant.address}
-                            onChange={onChangeHandler}
-                        />
-                    </div>
-                    <div className={styles["input"]}>
-                        <label htmlFor="phone-number" className={styles["phone"]}>Phone</label>
-                        <input
-                            type="text"
-                            className={styles["input-field"]}
-                            name="phone"
-                            id="phone-number"
-                            placeholder="+359888XXXXXX"
-                            value={restaurant.phone}
-                            onChange={onChangeHandler}
-                        />
-                    </div>
-                    <div className={styles["input"]}>
-                        <label htmlFor="restaurant-capacity" className={styles["capacity"]}>Capacity</label>
-                        <input
-                            type="text"
-                            className={styles["input-field"]}
-                            name="capacity"
-                            id="restaurant-capacity"
-                            placeholder="100 persons"
-                            value={restaurant.capacity}
-                            onChange={onChangeHandler}
-                        />
-                    </div>
-                    <div className={styles["input"]}>
-                        <label htmlFor="restaurant-image" className={styles["imageUrl"]}>Image</label>
-                        <input
-                            type="text"
-                            className={styles["input-field"]}
-                            name="imageUrl"
-                            id="restaurant-image"
-                            placeholder="http://image.jpeg"
-                            value={restaurant.imageUrl}
-                            onChange={onChangeHandler}
-                        />
-                    </div>
-                    <div className={styles["input"]}>
-                        <label htmlFor="restaurant-summary" className={styles["summary"]}>Summary</label>
-                        <input
-                            type="text"
-                            className={styles["input-field"]}
-                            name="summary"
-                            id="restaurant-summary"
-                            placeholder="Some text..."
-                            value={restaurant.summary}
-                            onChange={onChangeHandler}
-                        />
-                    </div>
-                    <div className={styles["action"]}>
-                        <button className={styles["action-button"]}>Edit</button>
-                    </div>
-                </form>
-
-            </div>
-        </section>
-    );
+            </section>
+        );
 }
--- src/contexts/LoadingContext.js
+++ src/contexts/LoadingContext.js
@@ -3,7 +3,7 @@ import { createContext, useState, useCallback } from "react";
 export const LoadingContext = createContext();
 
 export const LoadingProvider = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
     const showLoading = useCallback(() => {
         setIsLoading(true);
