e0445113135a9ba069281fc62adc2050e62de43a
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-21 17:21:53 +0200

    Add  inputs validation in Register page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -5,6 +5,7 @@ import styles from "./Auth.module.css";
 import { AuthContext } from "../../contexts/AuthContext";
 import { LoadingContext } from "../../contexts/LoadingContext";
 import * as authService from "../../services/authService";
+import * as validation from "../../validation";
 import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
 
 export const Register = () => {
@@ -14,6 +15,9 @@ export const Register = () => {
         password: '',
         rePass: ''
     });
+
+    const [errors, setErrors] = useState({});
+
     const { userLogin } = useContext(AuthContext);
     const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
     const navigate = useNavigate();
@@ -27,6 +31,12 @@ export const Register = () => {
 
     const onSubmit = async (e) => {
         e.preventDefault();
+
+        for (let key in errors) {
+            if (errors[key]) {
+                return;
+            }
+        }
 
         try {
             showLoading();
@@ -56,7 +66,7 @@ export const Register = () => {
                     </div>
                     <form onSubmit={onSubmit} className={styles["auth-form"]} >
                         <div className={styles["input"]}>
-                            <label htmlFor="full-name" className={styles["fullname"]}>Full Name</label>
+                            <label htmlFor="full-name" className={styles["fullname"]}>Full name</label>
                             <input
                                 type="text"
                                 className={styles["input-field"]}
@@ -65,8 +75,14 @@ export const Register = () => {
                                 placeholder="John Doe"
                                 value={inputs.fullname}
                                 onChange={onChangeHandler}
+                                onBlur={(e) => validation.isValidFullName(e, setErrors)}
                             />
                         </div>
+                        {errors.fullname &&
+                            <div className={styles["error-msg"]}>
+                                Full name must be at least two words starts with capital letter and separate with empty space or "-"!
+                            </div>
+                        }
                         <div className={styles["input"]}>
                             <label htmlFor="username" className={styles["username"]}>Username</label>
                             <input
@@ -77,8 +93,14 @@ export const Register = () => {
                                 placeholder="jdoe"
                                 value={inputs.username}
                                 onChange={onChangeHandler}
+                                onBlur={(e) => validation.minLength(e, setErrors, 3)}
                             />
                         </div>
+                        {errors.username &&
+                            <div className={styles["error-msg"]}>
+                                Username must be at least 3 symbols different than empty space!
+                            </div>
+                        }
                         <div className={styles["input"]}>
                             <label htmlFor="password" className={styles["password"]}>Password</label>
                             <input
@@ -89,10 +111,16 @@ export const Register = () => {
                                 placeholder="******"
                                 value={inputs.password}
                                 onChange={onChangeHandler}
+                                onBlur={(e) => validation.minLength(e, setErrors, 6)}
                             />
                         </div>
+                        {errors.password &&
+                            <div className={styles["error-msg"]}>
+                                Password must be at least 6 symbols different than empty space!
+                            </div>
+                        }
                         <div className={styles["input"]}>
-                            <label htmlFor="rePass" className={styles["re-password"]}>Repeat Password</label>
+                            <label htmlFor="rePass" className={styles["re-password"]}>Repeat password</label>
                             <input
                                 type="password"
                                 className={styles["input-field"]}
@@ -101,8 +129,15 @@ export const Register = () => {
                                 placeholder="******"
                                 value={inputs.rePass}
                                 onChange={onChangeHandler}
+                                onBlur={(e) => validation.isPaswordsMatch(e, inputs.password, setErrors)}
                             />
                         </div>
+                        {errors.rePass &&
+                            <div className={styles["error-msg"]}>
+                                Passwords don't match!
+                            </div>
+                        }
+
                         <div className={styles["action"]}>
                             <button className={styles["action-button"]}>Register</button>
                         </div>
---
+++ src/validation.js
@@ -0,0 +1,22 @@
+export const isValidFullName = (e, setError) => {
+    const pattern = /^([A-Z]{1}[a-z]*[ |-]{0,1})+[A-Z]{1}[a-z]*$/g;
+
+    setError(state => ({
+        ...state,
+        [e.target.name]: !pattern.test(e.target.value)
+    }));
+}
+
+export const minLength = (e, setError, length) => {
+    setError(state => ({
+        ...state,
+        [e.target.name]: e.target.value.length < length || e.target.value.includes(' ')
+    }));
+}
+
+export const isPaswordsMatch = (e, password, setError) => {
+    setError(state => ({
+        ...state,
+        [e.target.name]: e.target.value !== password
+    }));
+}
