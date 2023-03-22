4e32e40da97c8e484bd9b5e053530c7fffa753de
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-22 21:46:44 +0200

    Add Search component

---
+++ src/components/Restaurants/Search/Search.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import styles from "./Search.module.css";
+
+export const Search = ({ onSearch }) => {
+    const [searched, setSearched] = useState('');
+
+    const onChangeHandler = (e) => {
+        setSearched(e.target.value);
+    }
+
+    const onClearSearch = () => {
+        setSearched('')
+        onSearch('');
+    }
+
+    return (
+        <div className={styles['search-container']}>
+            <input
+                className={styles['search-input']}
+                name="search"
+                id="search-input"
+                value={searched}
+                onChange={onChangeHandler}
+            />
+            {searched.length > 0 &&
+                <button
+                    className={styles["btn-close"]}
+                    onClick={onClearSearch}
+                >
+                    <i className="fa-regular fa-rectangle-xmark"></i>
+                </button>
+            }
+            <button
+                className={styles['search-btn']}
+                onClick={() => onSearch(searched)}
+            >Search</button>
+        </div>
+    );
+}
