da3568fcdb0091125662bc82100cff204169a580
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-24 21:48:49 +0200

    Add favouritesService

---
+++ src/services/favouritesService.js
@@ -0,0 +1,10 @@
+import * as api from "./api";
+
+const endpoints = {
+    userFavourites: (ownerId) => `/data/favourites?where=_ownerId%3D%22${ownerId}%22`,
+    isRrestaurantInFavourites: (restaurantId) => `/data/favourites?where=restaurantId%3D%22${restaurantId}%22`
+}
+
+export const getUserFavourites = (ownerId) => api.get(endpoints.userFavourites(ownerId));
+
+export const getFavourites = (restaurantId) => api.get(endpoints.isRrestaurantInFavourites(restaurantId));
--- src/services/myProfileService.js
+++
@@ -1,7 +0,0 @@
-import * as api from "./api";
-
-const endpoints = {
-    getUserInfo: '/users/me'
-}
-
-export const getUserDetails = () => api.get(endpoints.getUserInfo);
