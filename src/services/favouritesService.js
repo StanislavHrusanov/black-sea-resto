df4740d4c8ba36d759b30798615bebd0627329e5
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-25 17:31:28 +0200

    Add functions in favouritesService to add and remove favourites

--- src/services/favouritesService.js
+++ src/services/favouritesService.js
@@ -2,9 +2,15 @@ import * as api from "./api";
 
 const endpoints = {
     userFavourites: (ownerId) => `/data/favourites?where=_ownerId%3D%22${ownerId}%22`,
-    isRrestaurantInFavourites: (restaurantId) => `/data/favourites?where=restaurantId%3D%22${restaurantId}%22`
+    isRrestaurantInFavourites: (restaurantId) => `/data/favourites?where=restaurantId%3D%22${restaurantId}%22`,
+    addToFavourites: '/data/favourites',
+    deleteFromFavourites: (favouriteId) => `/data/favourites/${favouriteId}`
 }
 
 export const getUserFavourites = (ownerId) => api.get(endpoints.userFavourites(ownerId));
 
 export const getFavourites = (restaurantId) => api.get(endpoints.isRrestaurantInFavourites(restaurantId));
+
+export const addToFavourites = (data) => api.post(endpoints.addToFavourites, data);
+
+export const removeFromFavourites = (favouriteId) => api.del(endpoints.deleteFromFavourites(favouriteId));
