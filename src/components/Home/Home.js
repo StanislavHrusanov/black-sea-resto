0efba09c555ba0ef94d93401fce89d939dcc4c4a
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-28 16:23:31 +0300

    Add functionality to show Last Added restaurant

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,6 +8,7 @@ import { getAllRestaurants } from "../../services/restaurantService";
 
 import { LoadingContext } from "../../contexts/LoadingContext";
 import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
+import { LastAdded } from "./LastAdded/LastAdded";
 
 export const Home = () => {
     const [restaurants, setRestaurants] = useState([]);
@@ -31,6 +32,8 @@ export const Home = () => {
         .sort((a, b) => b.reviews.length - a.reviews.length || getAvgRating(b.reviews) - getAvgRating(a.reviews))
         .slice(0, 3);
 
+    const lastAdded = restaurants.slice(0, 1);
+
     return isLoading
         ? (
             <LoadingSpinner />
@@ -40,15 +43,29 @@ export const Home = () => {
                 <div className={styles["home-message"]}>
                     <h2>We will help you find your perfect restaurant!</h2>
                 </div>
-                <div className={styles["home-container"]}>
+                <div className={styles["most-popular-container"]}>
 
-                    <div className={styles["top-three"]}>
+                    <div className={styles["most-popular"]}>
                         <h1>Most Popular</h1>
 
                         {
                             mostPopular.length > 0
                                 ? mostPopular.map(x => <Restaurant key={x._id} restaurant={x} />)
                                 : <p className={styles["no-restaurants"]}>There is no restaurants yet!</p>
+                        }
+
+                    </div>
+                </div>
+
+                <div className={styles["last-added-container"]}>
+
+                    <div className={styles["last-added"]}>
+                        <h1>Last added</h1>
+
+                        {
+                            lastAdded.length > 0
+                                ? <LastAdded key={lastAdded[0]._id} restaurant={lastAdded[0]} />
+                                : <p className={styles["no-restaurants"]}>There is no restaurants added yet!</p>
                         }
 
                     </div>
