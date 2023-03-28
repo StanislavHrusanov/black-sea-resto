dab49a83d0c6ac908455da20496991408d31e044
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-28 11:31:03 +0300

    Add function to get user restaurants

--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -5,7 +5,8 @@ const endpoints = {
     addRestaurant: '/data/restaurants',
     getRestaurant: (restaurantId) => `/data/restaurants/${restaurantId}`,
     editRestaurant: (restaurantId) => `/data/restaurants/${restaurantId}`,
-    deleteRestaurant: (restaurantId) => `/data/restaurants/${restaurantId}`
+    deleteRestaurant: (restaurantId) => `/data/restaurants/${restaurantId}`,
+    getUserRestaurants: (userId) => `/data/restaurants?where=_ownerId%3D%22${userId}%22`
 }
 
 export const getAllRestaurants = () => api.get(endpoints.allRestaurants);
@@ -17,3 +18,5 @@ export const getOne = (restaurantId) => api.get(endpoints.getRestaurant(restaura
 export const edit = (restaurantId, data) => api.put(endpoints.editRestaurant(restaurantId), data);
 
 export const del = (restaurantId) => api.del(endpoints.deleteRestaurant(restaurantId));
+
+export const getMyRestaurants = (userId) => api.get(endpoints.getUserRestaurants(userId));
