6d3f6dcca6336394a3f9469bb4f53073ab961515
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-29 12:00:56 +0300

    Add LastReview component

---
+++ src/components/Home/LastReview/LastReview.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import styles from "./LastReview.module.css";
+
+export const LastReview = ({review}) => {
+const rating = review.rating;
+const comment = review.comment !== '' ? review.comment : '(no comment)';
+
+    return (
+        <div className={styles["review"]}>
+            <p className={styles["desc"]}>Left for:</p>
+            <Link to={`/restaurants/${review.restaurantId}`}>
+                <h3>{review.restaurantName}</h3>
+            </Link>
+            <p className={styles["desc"]}>By:</p>
+            <h3>{review.author}</h3>
+            <p className={styles["desc"]}>Rating:</p>
+            <div className={styles["star-rating-container"]}>
+                <h2> <div className={styles["rating"]}>
+                    {[...Array(5)].map((star, index) => {
+                        index += 1;
+
+                        return (
+                            <span
+                                key={index}
+                                className={index <= rating ? styles["full"] : styles["empty"]}
+                            >
+                                ☆
+                            </span>
+                        )
+                    })}
+
+                </div>
+                </h2>
+            </div>
+
+            <p className={styles["desc"]}>Comment:</p>
+            <div className={styles["comment"]} >{comment}</div>
+        </div>
+    );
+}
