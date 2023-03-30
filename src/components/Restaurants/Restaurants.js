428dce6a27ab51e8cd716834b0c03eb92c0e327f
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-30 11:19:47 +0300

    Update search functionality

--- src/components/Restaurants/Restaurants.js
+++ src/components/Restaurants/Restaurants.js
@@ -4,7 +4,7 @@ import styles from "./Restaurants.module.css";
 
 import { RestaurantItem } from "./RestaurantItem/RestaurantItem";
 import { Search } from "./Search/Search";
-import { getAllRestaurants } from "../../services/restaurantService";
+import * as restaurantService from "../../services/restaurantService";
 
 import { LoadingContext } from "../../contexts/LoadingContext";
 import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
@@ -14,28 +14,30 @@ import * as utils from "../../utils";
 export const Restaurants = () => {
     const [restaurants, setRestaurants] = useState([]);
     const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-    const [search, setSearch] = useState('');
     const [sortCriteria, setSortCriteria] = useState('date');
     const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
     const navigate = useNavigate();
 
     useEffect(() => {
-        showLoading();
-        getAllRestaurants()
-            .then(result => setRestaurants(result))
-            .then(() => hideLoading())
-            .catch(err => {
-                window.alert(err.message);
+        (async () => {
+            try {
+                showLoading();
+                const allRestaurants = await restaurantService.getAllRestaurants();
+                setRestaurants(allRestaurants);
+                setFilteredRestaurants(allRestaurants);
+                hideLoading();
+
+            } catch (error) {
+                window.alert(error.message);
                 hideLoading();
                 return navigate('/restaurants');
-            });
+            }
+        })();
     }, [showLoading, hideLoading, navigate]);
 
-    const sortedRestaurants = utils.sortRestaurantsByCriteria(restaurants, sortCriteria);
     const sortedFilteedRestaurants = utils.sortRestaurantsByCriteria(filteredRestaurants, sortCriteria);
 
     const onSearch = (searched) => {
-        setSearch(searched);
         setFilteredRestaurants(restaurants.filter(x => x.name.toLowerCase().includes(searched.toLowerCase())));
     }
 
@@ -74,11 +76,9 @@ export const Restaurants = () => {
 
                         {restaurants.length === 0
                             ? <p className={styles["no-restaurants"]}>There is no restaurants yet!</p>
-                            : search === ''
-                                ? sortedRestaurants.map(x => <RestaurantItem key={x._id} restaurant={x} />)
-                                : sortedFilteedRestaurants.length === 0
-                                    ? <p className={styles["no-restaurants"]}>0 restaurants found!</p>
-                                    : sortedFilteedRestaurants.map(x => <RestaurantItem key={x._id} restaurant={x} />)
+                            : sortedFilteedRestaurants.length === 0
+                                ? <p className={styles["no-restaurants"]}>0 restaurants found!</p>
+                                : sortedFilteedRestaurants.map(x => <RestaurantItem key={x._id} restaurant={x} />)
                         }
 
                     </div>
