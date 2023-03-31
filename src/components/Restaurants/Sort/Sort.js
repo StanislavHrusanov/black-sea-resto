3286a05856b0aa1da6364bb629b73bb6dcd3914b
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-31 09:43:56 +0300

    Move sortBy to Sort component and Sort.module.css

---
+++ src/components/Restaurants/Sort/Sort.js
@@ -0,0 +1,24 @@
+import styles from "./Sort.module.css";
+
+export const Sort = ({ sortCriteria, onSort }) => {
+
+    return (
+        <div className={styles["sort-container"]}>
+            <div className={styles["sort-div"]}>
+                <span>Sort by</span>
+                <select
+                    value={sortCriteria}
+                    name="criteria"
+                    className={styles["criteria"]}
+                    onChange={(e) => onSort(e.target.value)}
+                >
+                    <option value=""></option>
+                    <option value="newest">Newest</option>
+                    <option value="a-z">A-Z</option>
+                    <option value="reviews">Reviews</option>
+                    <option value="rating">Rating</option>
+                </select>
+            </div>
+        </div>
+    );
+}
