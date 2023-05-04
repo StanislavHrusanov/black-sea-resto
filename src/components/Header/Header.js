bf8350fca493465f49868d46a2e886dbc2bef5de
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-05-04 15:41:58 +0300

    Update on navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,34 +8,32 @@ export const Header = () => {
     const [isNavToggled, setIsNavToggled] = useState(false);
     const { user } = useContext(AuthContext);
 
-
     return (
         <header className={styles["header"]}>
             <h1><Link className={styles["home"]} to="/">Black Sea Resto</Link></h1>
 
             <button onClick={() => setIsNavToggled(!isNavToggled)} className={styles["btn-mobile-menu"]}><i className="fas fa-bars"></i></button>
 
-            {isNavToggled &&
-                <nav className={styles["nav"]}>
-                    {/* {user &&
+
+            <nav className={styles[isNavToggled ? "nav" : "hidden"]}>
+                {/* {user &&
                     <span className={styles["hello"]}>{`Hello, ${user.username}!`}</span>
                 } */}
-                    <Link className={styles["nav-link"]} to="/">Home</Link>
-                    <Link className={styles["nav-link"]} to="/restaurants">Restaurants</Link>
-                    {user
-                        ? <div id="user">
-                            <Link className={styles["nav-link"]} to="/addRestaurant">Add restaurant</Link>
-                            <Link className={styles["nav-link"]} to="/myProfile">My profile</Link>
-                            <Link className={styles["nav-link"]} to="/logout">Logout</Link>
-                        </div>
+                <Link className={styles["nav-link"]} to="/restaurants">Restaurants</Link>
+                {user
+                    ? <div id="user">
+                        <Link className={styles["nav-link"]} to="/addRestaurant">Add restaurant</Link>
+                        <Link className={styles["nav-link"]} to="/myProfile">My profile</Link>
+                        <Link className={styles["nav-link"]} to="/logout">Logout</Link>
+                    </div>
 
-                        : <div id="guest">
-                            <Link className={styles["nav-link"]} to="/login">Login</Link>
-                            <Link className={styles["nav-link"]} to="/register">Register</Link>
-                        </div>
-                    }
-                </nav>
-            }
+                    : <div id="guest">
+                        <Link className={styles["nav-link"]} to="/login">Login</Link>
+                        <Link className={styles["nav-link"]} to="/register">Register</Link>
+                    </div>
+                }
+            </nav>
+
         </header>
     );
 }
