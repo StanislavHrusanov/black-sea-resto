ec7a6831f1c7bf22f70295978795411904e7c82c
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-05-05 15:47:42 +0300

    CSS updates for mobile responsive design

--- src/components/Home/LastReview/LastReview.js
+++ src/components/Home/LastReview/LastReview.js
@@ -1,40 +1,48 @@
 import { Link } from "react-router-dom";
 import styles from "./LastReview.module.css";
 
-export const LastReview = ({review}) => {
-const rating = review.rating;
-const comment = review.comment !== '' ? review.comment : '(no comment)';
+export const LastReview = ({ review }) => {
+    const rating = review.rating;
+    const comment = review.comment !== '' ? review.comment : '(no comment)';
 
     return (
         <div className={styles["review"]}>
-            <p className={styles["desc"]}>Left for:</p>
-            <Link to={`/restaurants/${review.restaurantId}`}>
-                <h3>{review.restaurantName}</h3>
-            </Link>
-            <p className={styles["desc"]}>By:</p>
-            <h3>{review.author}</h3>
-            <p className={styles["desc"]}>Rating:</p>
-            <div className={styles["star-rating-container"]}>
-                <h2> <div className={styles["rating"]}>
-                    {[...Array(5)].map((star, index) => {
-                        index += 1;
+            <div className={styles["desc"]}>
+                <p>Left for:</p>
+                <Link to={`/restaurants/${review.restaurantId}`}>
+                    <h3>{review.restaurantName}</h3>
+                </Link>
+            </div>
+            <div className={styles["desc"]}>
+                <p>By:</p>
+                <h3>{review.author}</h3>
+            </div>
+            <div className={styles["desc"]}>
+                <p>Rating:</p>
+                <div className={styles["star-rating-container"]}>
+                    <h2> <div className={styles["rating"]}>
+                        {[...Array(5)].map((star, index) => {
+                            index += 1;
 
-                        return (
-                            <span
-                                key={index}
-                                className={index <= rating ? styles["full"] : styles["empty"]}
-                            >
-                                ☆
-                            </span>
-                        )
-                    })}
+                            return (
+                                <span
+                                    key={index}
+                                    className={index <= rating ? styles["full"] : styles["empty"]}
+                                >
+                                    ☆
+                                </span>
+                            )
+                        })}
 
+                    </div>
+                    </h2>
                 </div>
-                </h2>
             </div>
 
-            <p className={styles["desc"]}>Comment:</p>
-            <div className={styles["comment"]} >{comment}</div>
+            <div className={styles["desc"]}>
+                <p>Comment:</p>
+                <p className={styles["comment"]} >{comment}</p>
+            </div>
         </div>
     );
 }
