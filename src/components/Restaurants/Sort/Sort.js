3c10fb056733f528d18d43e75d8de66fb36875b6
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-05-09 17:17:02 +0300

    Update Sort to be responsive

--- src/components/Restaurants/Sort/Sort.js
+++ src/components/Restaurants/Sort/Sort.js
@@ -3,22 +3,20 @@ import styles from "./Sort.module.css";
 export const Sort = ({ sortCriteria, onSort }) => {
 
     return (
-        <div className={styles["sort-container"]}>
-            <div className={styles["sort-div"]}>
-                <span>Sort by</span>
-                <select
-                    value={sortCriteria}
-                    name="criteria"
-                    className={styles["criteria"]}
-                    onChange={(e) => onSort(e.target.value)}
-                >
-                    <option value=""></option>
-                    <option value="newest">Newest</option>
-                    <option value="a-z">A-Z</option>
-                    <option value="reviews">Reviews</option>
-                    <option value="rating">Rating</option>
-                </select>
-            </div>
+        <div className={styles["sort-div"]}>
+            <span>Sort by</span>
+            <select
+                value={sortCriteria}
+                name="criteria"
+                className={styles["criteria"]}
+                onChange={(e) => onSort(e.target.value)}
+            >
+                <option value=""></option>
+                <option value="newest">Newest</option>
+                <option value="a-z">A-Z</option>
+                <option value="reviews">Reviews</option>
+                <option value="rating">Rating</option>
+            </select>
         </div>
     );
 }
