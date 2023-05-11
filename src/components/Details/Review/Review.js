9c6dc33e370fd09ff2aadd1c141436e25eb34ff2
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-05-11 13:38:47 +0300

    Update reviews to be mobile responsive

--- src/components/Details/Review/Review.js
+++ src/components/Details/Review/Review.js
@@ -4,36 +4,36 @@ import { commentDateConverter } from "../../../utils";
 export const Review = ({ review }) => {
     const rating = review.rating;
     const comment = review.comment;
-    
+
     return (
         <div className={styles["review-container"]}>
-            <p >{review.author}</p>
-            <p className={styles["posted"]}>
-                {review._updatedOn
-                    ? `edited at: ${commentDateConverter(review._updatedOn)}`
-                    : `posted at: ${commentDateConverter(review._createdOn)}`}
-            </p>
-            <div className={styles["review-body"]}>
-                <div className={styles["star-rating-container"]}>
-                    <h3><div className={styles["rating"]}>
-                        {[...Array(5)].map((star, index) => {
-                            index += 1;
-
-                            return (
-                                <span
-                                    key={index}
-                                    className={index <= rating ? styles["full"] : styles["empty"]}
-                                >
-                                    ☆
-                                </span>
-                            )
-                        })}
-
-                    </div>
-                    </h3>
-                </div>
-                <div className={styles["comment"]} >{comment}</div>
+            <div className={styles["review-author-date"]}>
+                <p className={styles["author"]}>{review.author}</p>
+                <p className={styles["posted"]}>
+                    {review._updatedOn
+                        ? `edited at: ${commentDateConverter(review._updatedOn)}`
+                        : `posted at: ${commentDateConverter(review._createdOn)}`}
+                </p>
             </div>
+
+            <h3><div className={styles["rating"]}>
+                {[...Array(5)].map((star, index) => {
+                    index += 1;
+
+                    return (
+                        <span
+                            key={index}
+                            className={index <= rating ? styles["full"] : styles["empty"]}
+                        >
+                            ☆
+                        </span>
+                    )
+                })}
+
+            </div>
+            </h3>
+
+            <div className={styles["comment"]} >{comment}</div>
 
         </div>
     );
